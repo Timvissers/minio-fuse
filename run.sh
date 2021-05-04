fb042ffb0e82ea0b5953aef8cd4fca001e47ea53
Tim Vissers <tim.vissers@collibra.com>
2021-05-04 16:43:07 +0200

    fixes

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+echo "Setting credentials"
 echo "$MINIO_ACCESS_KEY:$MINIO_SECRET_KEY" > passwd && chmod 600 passwd
+echo "starting s3fs"
 s3fs "$MINIO_BUCKET_NAME" "$MNT_POINT" -o passwd_file=passwd -o url=$MINIO_URL -o use_path_request_style
+echo "s3fs running"
+
+while true; do sleep 30; done;
